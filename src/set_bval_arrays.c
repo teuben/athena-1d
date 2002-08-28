e7a8a31f3bb833e77c354b6dd07ebac14272ea22
gardiner <gardiner>
2002-08-28 15:27:16 +0000

    I've changed this routine to use a switch statement instead of multiple if's when setting the variable ii.  This has the benefit that a minimum number of tests are made and a default was included to throw an error should the values of agrid->niib or agrid->noib be set improperly.
    I've left the old code in place and commented out for reference.

--- src/set_bval_arrays.c
+++ src/set_bval_arrays.c
@@ -15,58 +15,84 @@ void set_bval_arrays(struct grid_block *agrid, struct bval_array *bval)
 
 /*------------------------ Start with Inner I boundary -----------------------*/
 
-   if (agrid->niib == 1) {reflect_s = -1.0;} else {reflect_s = 1.0;}
+  if (agrid->niib == 1) {reflect_s = -1.0;} else {reflect_s = 1.0;}
 
 /* Do niib=3 (inflow) */
 
-   if (agrid->niib == 3) {
-      for (i=0;  i<=3;  i++) {
+  if (agrid->niib == 3) {
+    for (i=0;  i<=3;  i++) {
       for (n=0;  n<NVAR;  n++) {
-         bval->uiib[i][n] = agrid->boundary_values.uiib[i][n];
-         bval->bxiib[i] = agrid->boundary_values.bxiib[i];
-      }}
+	bval->uiib[i][n] = agrid->boundary_values.uiib[i][n];
+	bval->bxiib[i] = agrid->boundary_values.bxiib[i];
+      }
+    }
 
 /* Do niib=1 (reflection), 2 (flow-out), 4 (periodic) by adjusting i-index   */
 
-   } else {
-      for (i=0;  i<=3;  i++) {
-         if (agrid->niib == 1) {ii = is+(3-i);}
-         if (agrid->niib == 2) {ii = is;}
-         if (agrid->niib == 4) {ii = ie-(3-i);}
-         for (n=0;  n<NVAR;  n++) {
-            bval->uiib[i][n] = agrid->u[ii][n];
-            bval->bxiib[i] = agrid->bx[ii];
-         }
-         bval->uiib[i][1] = agrid->u[ii][1]*reflect_s;
+  } else {
+    for (i=0;  i<=3;  i++) {
+      switch(agrid->niib){
+      case 1:
+	ii = is+(3-i);  break;
+      case 2:
+	ii = is;  break;
+      case 4:
+	ii = ie-(3-i);  break;
+      default:
+	fprintf(stderr,"[set_bval_arrays]: agrid->niib = %d unknown\n",
+		agrid->niib);
+	ii = is; /* Choose outflow as default */
       }
-   }
+      /* if (agrid->niib == 1) {ii = is+(3-i);}
+	 if (agrid->niib == 2) {ii = is;}
+	 if (agrid->niib == 4) {ii = ie-(3-i);} */
+      for (n=0;  n<NVAR;  n++) {
+	bval->uiib[i][n] = agrid->u[ii][n];
+	bval->bxiib[i] = agrid->bx[ii];
+      }
+      bval->uiib[i][1] = agrid->u[ii][1]*reflect_s;
+    }
+  }
 
 /*----------------------- Continue with Outer I boundary ---------------------*/
 
-   if (agrid->noib == 1) {reflect_s = -1.0;} else {reflect_s = 1.0;}
+  if (agrid->noib == 1) {reflect_s = -1.0;} else {reflect_s = 1.0;}
 
 /* Do noib=3 (inflow) */
 
-   if (agrid->noib == 3) {
-      for (i=0;  i<=3;  i++) {
+  if (agrid->noib == 3) {
+    for (i=0;  i<=3;  i++) {
       for (n=0;  n<NVAR;  n++) {
-         bval->uoib[i][n] = agrid->boundary_values.uoib[i][n];
-         bval->bxoib[i] = agrid->boundary_values.bxoib[i];
-      }}
+	bval->uoib[i][n] = agrid->boundary_values.uoib[i][n];
+	bval->bxoib[i] = agrid->boundary_values.bxoib[i];
+      }
+    }
 
 /* Do noib=1 (reflection), 2 (flow-out), 4 (periodic) by adjusting i-index   */
 /* Note Bx ghost zones start at ie+2 rather than ie+1 */
 
-   } else {
-      for (i=0;  i<=3;  i++) {
-         if (agrid->noib == 1) {ii = ie-i;}
-         if (agrid->noib == 2) {ii = ie;}
-         if (agrid->noib == 4) {ii = is+i;}
-         for (n=0;  n<NVAR;  n++) {
-            bval->uoib[i][n] = agrid->u[ii][n];
-            bval->bxoib[i] = agrid->bx[ii+1];
-         }
-         bval->uoib[i][1] = agrid->u[ii][1]*reflect_s;
+  } else {
+    for (i=0;  i<=3;  i++) {
+      switch(agrid->noib){
+      case 1:
+	ii = ie-i;  break;
+      case 2:
+	ii = ie;  break;
+      case 4:
+	ii = is+i;  break;
+      default:
+	fprintf(stderr,"[set_bval_arrays]: agrid->noib = %d unknown\n",
+		agrid->noib);
+	ii = ie; /* Choose outflow as default */
       }
-   }
+      /* if (agrid->noib == 1) {ii = ie-i;}
+	 if (agrid->noib == 2) {ii = ie;}
+	 if (agrid->noib == 4) {ii = is+i;} */
+      for (n=0;  n<NVAR;  n++) {
+	bval->uoib[i][n] = agrid->u[ii][n];
+	bval->bxoib[i] = agrid->bx[ii+1];
+      }
+      bval->uoib[i][1] = agrid->u[ii][1]*reflect_s;
+    }
+  }
 }
