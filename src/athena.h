604e5c5e01dc0af9d4da996ebfbc0f80f00d56f5
gardiner <gardiner>
2002-10-08 15:55:39 +0000

    I've modified the definitions of the grid_block and bval_array structures. First, I've wrapped them in a typedef to define the types Grid_Block and Bval_Array.  Second, I've begun to fill in their definition for 1D, 2D, and 3D calculations.  The cell average magnetic fields are stored in the vector of conserved variables.  The interface magnetic fields are stored in separate arrays with the same number of elements as the grid cells.  The interface magnetic fields are located at the "right" side of the grid cell.  Right in this context is the side of the grid cell at largest x-, y-, or z-coordinate.

--- src/athena.h
+++ src/athena.h
@@ -5,19 +5,53 @@
 
 typedef double Real;
 
-struct bval_array{
-  Real uiib[4][NVAR], uoib[4][NVAR], bxiib[4], bxoib[4];
-};
+typedef struct bval_array{
+#if defined ONE_D
+  Real uiib[4][NVAR], uoib[4][NVAR];
+#ifdef MHD
+  Real bxiib[4], bxoib[4];
+#endif /* MHD */
 
-struct grid_block{
-  Real u[NX1][NVAR],bx[NX1+1];
-  Real dx,dt,time;
+#elif defined TWO_D
+#error: 2D bval_array is not yet defined
+
+#else /* THREE_D */
+#error: 3D bval_array is not yet defined
+
+#endif
+}Bval_Array;
+
+
+
+typedef struct grid_block{
+#if defined ONE_D
+  Real dx;
+  Real u[NX1][NVAR];
+#ifdef MHD
+  Real bx[NX1];
+#endif /* MHD */
+
+#elif defined TWO_D
+  Real dx,dy;
+  Real u[NX1][NX2][NVAR];
+#ifdef MHD
+  Real bx[NX1][NX2],by[NX1][NX2];
+#endif /* MHD */
+
+#else /* THREE_D */
+  Real dx,dy,dz;
+  Real u[NX1][NX2][NX3][NVAR];
+#ifdef MHD
+  Real bx[NX1][NX2][NX3],by[NX1][NX2][NX3],bz[NX1][NX2][NX3];
+#endif /* MHD */
+#endif
+  Real dt,time;
   Real t_bin,dt_bin,t_hst,dt_hst,t_hdf,dt_hdf;
   struct bval_array boundary_values;
   int is,ie;
   int niib,noib;
   int ncycles;
   char bin_file[9],hst_file[9],hdf_file[9];
-};
+}Grid_Block;
 
 #endif /* ATHENA_H */
