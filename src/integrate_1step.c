a8da8e115166b01ac2c7a7feafafc5d262fd1954
gardiner <gardiner>
2002-10-09 16:47:00 +0000

    When extracting the 1D pencil of the conserved variables, we retain the cell averaged Bx.  However, the left and right states lack the cell averaged value of Bx since the appropriate value of Bx is then stored in the b_par array.
    The dimensions of the left and right states, the flux and the matrices of
    right and left eigenvectors is now NWAVE.

--- src/integrate_1step.c
+++ src/integrate_1step.c
@@ -23,45 +23,51 @@ void integrate_1step(struct grid_block *agrid)
   int i=0,n,is,ie;
   Real dtodx;
   Real maxev_lr,maxev_roe,maxev_xswp=0.0,min_dtx=1.0e37;
-  Real u[NXMAX][NVAR],wl[NXMAX][NVAR],wr[NXMAX][NVAR],f_u[NXMAX][NVAR];
-  Real b_par[NXMAX+1];
+  Real u[NXMAX][NVAR],wl[NXMAX][NWAVE],wr[NXMAX][NWAVE],f_u[NXMAX][NWAVE];
+  Real b_par[NXMAX];
 
-   is = agrid->is;
-   ie = agrid->ie;
-   dtodx = agrid->dt/agrid->dx;
+  is = agrid->is;
+  ie = agrid->ie;
+  dtodx = agrid->dt/agrid->dx;
 
 /* Extract 1-D pencil, packing variables into U[i] in correct order.  For */
-/* X1-sweep: U = (d, Sx, Sy, Sz, E, By, Bz) */
+/* X1-sweep: U = (d, Sx, Sy, Sz, E, Bx, By, Bz) */
 
-   for (i=is-3; i<=ie+3; i++) {
-      for (n=0; n<NVAR; n++) {
-         u[i][n] = agrid->u[i][n];
-      }
+  for (i=is-3; i<=ie+3; i++) {
+    for (n=0; n<NVAR; n++) {
+      u[i][n] = agrid->u[i][n];
+    }
 #ifdef MHD
-      b_par[i] = agrid->bx[i];
+    b_par[i] = agrid->bx[i];
 #endif
-   }
-#ifdef MHD
-   b_par[ie+4] = agrid->bx[ie+4];
-#endif
+  }
 
 /* Compute L and R states and fluxes, save maximum eigenvalue, and update */
 /* hydro variables */
 
-   maxev_lr  = lr_states (u,b_par,dtodx,is,ie,wl,wr    );
-   maxev_roe = roe_fluxes(wl,wr,b_par,  is,ie,f_u);
-   maxev_xswp = MAX(maxev_xswp,(MAX(maxev_lr,maxev_roe)));
+  maxev_lr  = lr_states (u,b_par,dtodx,is,ie,wl,wr);
+  maxev_roe = roe_fluxes(wl,wr,b_par,is,ie,f_u);
+  maxev_xswp = MAX(maxev_xswp,(MAX(maxev_lr,maxev_roe)));
 
-   for (i=is; i<=ie; i++) {
-   for (n=0; n<NVAR; n++) {
+  for (i=is; i<=ie; i++) {
+#ifdef MHD
+    for (n=0; n<NVAR-3; n++) {
       agrid->u[i][n] -= dtodx*(f_u[i+1][n] - f_u[i][n]);
-   }}
+    }
+    agrid->u[i][NVAR-2] -= dtodx*(f_u[i+1][NWAVE-2] - f_u[i][NWAVE-2]);
+    agrid->u[i][NVAR-1] -= dtodx*(f_u[i+1][NWAVE-1] - f_u[i][NWAVE-1]);
+#else
+    for (n=0; n<NVAR; n++) {
+      agrid->u[i][n] -= dtodx*(f_u[i+1][n] - f_u[i][n]);
+    }
+#endif
+  }
 
-   min_dtx = agrid->dx/maxev_xswp;
+  min_dtx = agrid->dx/maxev_xswp;
 
 /*================================= Finish up  ===============================*/
 
-   agrid->time = agrid->time + agrid->dt;
-   agrid->ncycles = agrid->ncycles + 1;
-   agrid->dt = COURNO*min_dtx;
+  agrid->time = agrid->time + agrid->dt;
+  agrid->ncycles = agrid->ncycles + 1;
+  agrid->dt = COURNO*min_dtx;
 }
