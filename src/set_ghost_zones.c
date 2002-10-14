8bf9b0474a6a76cbf43c2ee78f5f060cdb217bb9
gardiner <gardiner>
2002-10-14 21:21:18 +0000

    I've added a 2D version of the set_ghost_zones routine to the set_ghost_zones.c file which is selected depending on whether ONE_D or TWO_D is defined.
    NOTE: This 2D routine, like the 1D routine, assumes that is1 and is2 are
    both equal to 4 rather than reference these elements of the grid as
    u[is1 - 4 + i][j].

--- src/set_ghost_zones.c
+++ src/set_ghost_zones.c
@@ -5,6 +5,8 @@
 #include "athena.def"
 #include "athena.h"
 
+#if defined ONE_D
+
 void set_ghost_zones(struct grid_block *agrid, struct bval_array *bval)
 {
   int i=0,n;
@@ -23,3 +25,50 @@ void set_ghost_zones(struct grid_block *agrid, struct bval_array *bval)
     agrid->bx[ie+(i+1)] = bval->bxoib[i];
   }
 }
+
+#elif defined TWO_D
+
+void set_ghost_zones(struct grid_block *agrid, struct bval_array *bval){
+  int i,j,n;
+  int is1,is2,ie1,ie2;
+
+  is1 = agrid->is1;  ie1 = agrid->ie1;
+  is2 = agrid->is2;  ie2 = agrid->ie2;
+
+/*========================= Inner/Outer 1 boundaries =========================*/
+
+  for(i=0; i<4; i++){
+    for(j=is2-4; j<=ie2+4; j++){
+      for(n=0; n<NVAR; n++){
+	agrid->u[i][j][n] = bval->uiib1[i][j][n];
+	agrid->u[ie1+(i+1)][j][n] = bval->uoib1[i][j][n];
+      }
+      agrid->bx[i][j] = bval->bxiib1[i][j];
+      agrid->bx[ie1+(i+1)][j] = bval->bxoib1[i][j];
+
+      agrid->by[i][j] = bval->byiib1[i][j];
+      agrid->by[ie1+(i+1)][j] = bval->byoib1[i][j];
+    }
+  }
+
+/*========================= Inner/Outer 2 boundaries =========================*/
+
+  for(i=is1-4; i<=ie1+4; i++){
+    for(j=0; j<4; j++){
+      for(n=0; n<NVAR; n++){
+	agrid->u[i][j][n] = bval->uiib2[i][j][n];
+	agrid->u[i][ie2+(j+1)][n] = bval->uoib2[i][j][n];
+      }
+      agrid->bx[i][j] = bval->bxiib2[i][j];
+      agrid->bx[i][ie2+(j+1)] = bval->bxoib2[i][j];
+
+      agrid->by[i][j] = bval->byiib2[i][j];
+      agrid->by[i][ie2+(j+1)] = bval->byoib2[i][j];
+    }
+  }
+}
+
+#else /* THREE_D */
+
+
+#endif
