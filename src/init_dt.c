174b4e3fdc1a575e684653acd35ea9f0369403d5
gardiner <gardiner>
2002-10-21 18:20:38 +0000

    I've implemented a simple extension of the 1D init_dt() function for the 2-D grid.

--- src/init_dt.c
+++ src/init_dt.c
@@ -8,6 +8,8 @@
 #include "athena.def"
 #include "athena.h"
 
+#if defined ONE_D
+
 Real init_dt(struct grid_block *agrid)
 {
   int i;
@@ -43,3 +45,49 @@ Real init_dt(struct grid_block *agrid)
   dt = COURNO*agrid->dx/cmax; 
   return(dt);
 }
+
+#elif defined TWO_D
+
+Real init_dt(Grid_Block *agrid){
+  int i,j;
+  Real d,v1,v2,v3,vmax,b1,b2,b3,bsq,va,qsq,p,cs,cmax=0.0,dt;
+
+  for (i=agrid->is1; i<=agrid->ie1; i++) {
+    for (j=agrid->is2; j<=agrid->ie2; j++) {
+      d = agrid->u[i][j][0];
+      v1 = agrid->u[i][j][1]/agrid->u[i][j][0];
+      v2 = agrid->u[i][j][2]/agrid->u[i][j][0];
+      v3 = agrid->u[i][j][3]/agrid->u[i][j][0];
+      vmax = MAX(fabs(v1),MAX(fabs(v2),fabs(v3)));
+#ifdef MHD
+      b1 = agrid->bx[i][j];
+      b2 = agrid->by[i][j];
+      b3 = agrid->u[i][j][NVAR-1];
+      bsq = b1*b1 + b2*b2 + b3*b3;
+      va = sqrt(bsq/d);
+#else
+      bsq = 0.0;
+      va = 0.0;
+#endif
+#ifdef ADIABATIC
+      qsq = v1*v1 + v2*v2 + v3*v3;
+      p = MAX( GAMM1*(agrid->u[i][j][4] - 0.5*d*qsq - 0.5*bsq), TINY_NUMBER);
+      cs = sqrt(GAMMA*p/d);
+#else
+      cs = ISOTHERMAL_C;
+#endif
+      cmax = MAX(cmax, cs + va + vmax);
+    }
+  }
+
+  printf("cmax = %e, dx = %e, dy = %e\n",cmax,agrid->dx,agrid->dy);
+  dt = COURNO*(agrid->dx < agrid->dy ? agrid->dx : agrid->dy)/cmax; 
+
+  return dt;
+}
+
+#else /* THREE_D */
+
+#error : 3-D version of init_dt() not yet implemented
+
+#endif
